882c6bfe7072ced37b0f732703c8c9ef155dd3dc
mrun <mrun@mrundeMacBook-Pro.local>
2018-06-07 00:44:09 +0800

    init

---
+++ elementary_list/main.c
@@ -0,0 +1,25 @@
+//
+// Created by mrun on 2018/6/6.
+//
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+}
+
+
+link new_list()
+{
+    int tmp[10];
+    link t;
+    srand(8);
+    t.item = rand();
+    t.next = null;
+    for (int i = 0; i < 10; ++i) {
+
+    }
+}
---
+++ elementary_list/main.h
@@ -0,0 +1,15 @@
+//
+// Created by mrun on 2018/6/6.
+//
+
+typedef struct node link
+
+struct node
+{
+    int item;
+    link *next;
+};
+
+link new_node(int);
+link new_list(void);
+void print_list(link*);
