d67692bd93bac25a5e92641f2ddd14ad8a59aa5b
NilFoss <github@coolcoder.site>
2018-06-20 12:01:45 +0800

    删除

--- elementary_mini_list/main.h
+++ elementary_mini_list/main.h
@@ -2,11 +2,6 @@
 // Created by mrun on 2018/6/18.
 //
 
-#ifndef ELEMENTARY_MINI_LIST_MAIN_H
-#define ELEMENTARY_MINI_LIST_MAIN_H
-
-#endif //ELEMENTARY_MINI_LIST_MAIN_H
-
 typedef struct node link;
 
 struct node
